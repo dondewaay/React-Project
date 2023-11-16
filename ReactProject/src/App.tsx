c6c2cfd87a71637feb055ddc73dfe338ae00ff68
ddewaay <ddewaay@uiowa.edu>
2023-11-16 11:39:05 -0600

    Added components and folders for header object

--- ReactProject/src/App.tsx
+++ ReactProject/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    <>
-      <h1> Hello World </h1>
-    </>
+    <div>
+      <Header/>
+    </div>
   );
 }
 
