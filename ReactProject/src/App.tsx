309c7f40d3143e5b48b80b8e40473c54dabdab85
ddewaay <ddewaay@uiowa.edu>
2023-11-16 15:04:28 -0600

    Components, navbar

--- ReactProject/src/App.tsx
+++ ReactProject/src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div>
-      <Header/>
-    </div>
+    <>
+      <Header /> <Button> Click for bears </Button>
+    </>
   );
 }
 
--- ReactProject/src/Components/Header/Header.tsx
+++ ReactProject/src/Components/Header/Header.tsx
@@ -1,7 +1,30 @@
 import "./Header.css";
+import Home from "../Home/Home";
+import About from "../About/About";
+import Button from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => {
-  return <div> Hewwo </div>;
+  return (
+    <>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">BEAR WEBSITE !!</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Bears</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Nav.Link href="#home">Credits</Nav.Link>
+      </Container>
+    </Navbar>
+    </>
+  );
 };
 
 export default Header;
