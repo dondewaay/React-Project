7443dd7e11edd2d1a3c01a3024ef06bd37f3ac82
ddewaay <ddewaay@uiowa.edu>
2023-11-18 22:24:54 -0600

    Bear Card (tm)

--- ReactProject/src/Components/Header/Header.tsx
+++ ReactProject/src/Components/Header/Header.tsx
@@ -10,19 +10,19 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 const Header = () => {
   return (
     <>
-    <Navbar expand="lg" className="navbar navbar-dark bg-dark">
-      <Container>
-        <Navbar.Brand href="#home">BEAR WEBSITE !!</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Bears</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Nav.Link href="#home">Credits</Nav.Link>
-      </Container>
-    </Navbar>
+      <Navbar expand="lg" className="navbar navbar-dark bg-dark">
+        <Container>
+          <Navbar.Brand href="#home">BEAR WEBSITE!</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">Bears</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Nav.Link href="#home">Credits</Nav.Link>
+        </Container>
+      </Navbar>
     </>
   );
 };
